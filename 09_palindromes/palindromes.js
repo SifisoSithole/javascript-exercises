2bf78514cca0a718d4363f9972e889eb7b83ad9f
SifisoSithole <sitholesifisobrian@gmail.com>
2023-11-06 12:45:49 +0200

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+function removePunctuation(text) {
+    let punctuation = /[\.,?! ]/g;
+    let newText = text.replace(punctuation, "");
+    return newText;
+}
 
+const palindromes = function (str) {
+    let newStr = str.toLowerCase();
+    newStr = removePunctuation(newStr);
+    let leftIndex = newStr.length - 1;
+
+    for (let rightIndex = 0; rightIndex < leftIndex; rightIndex++, leftIndex--){
+        if (newStr[rightIndex] !== newStr[leftIndex])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
